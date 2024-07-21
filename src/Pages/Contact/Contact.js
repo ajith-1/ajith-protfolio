25e68c571c30738553ef15ab79ce5a7e4bf7ec9a
ajith-1 <ajithm0216@gmail.com>
2024-07-21 12:55:06 +0530

    changes

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -7,21 +7,12 @@ import TextField from '@mui/material/TextField';
 function Contact() {
   return (
     <>
-     <Container id='contact' sx={{display:'flex',alignItems:'cener',justifyContent:'center',height:'100vh',flexDirection:'row'}}>
-        <Box>
-
-        </Box>
-        <Box component="form"
-      sx={{'& > :not(style)': { m: 1, width: '35ch' },display:'flex',flexDirection:'colomn'}}
-      noValidate
-      autoComplete="off">
-        <TextField id="outlined-basic" label="Name" variant="outlined" />
-        <TextField id="outlined-basic" label="Email" variant="outlined" /> 
-        <TextField id="outlined-basic" label="Message" variant="outlined" multiline rows={6}/>
-        </Box>
+      <Container id='about' sx={{display:'flex',alignItems:'cener',justifyContent:'center',height:'100vh',flexDirection:'column'}}>
+        <Typography variant='h2' textAlign={'center'} data-aos="fade-up" data-aos-duration='800'>Contact</Typography> 
+       
       </Container>
     </>
   )
 }
 
-export default Contact
+export default Contact;
